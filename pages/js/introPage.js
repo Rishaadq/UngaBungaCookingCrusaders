1fccb58371a20b782fb34870e697c0b68fbf73e8
Rishaadq <rishaadq@gmail.com>
2022-07-19 23:45:03 +0300

    Added onclick scrolling back to the top of each recipe page when clicking anchor text

---
+++ pages/js/introPage.js
@@ -0,0 +1,28 @@
+
+function createRecipeList(recipeListCollection, recipes) {
+    for(i= 0; i < recipeListCollection.length; i++) {
+        let recipeInfo = recipeListCollection[i];
+        let recipeName = recipeInfo.children[1].textContent;
+        var obj = {image: "", name: recipeName, element: recipeInfo };
+        recipes.push(obj); 
+    }
+    return recipes;
+}
+
+function recipeSearch(searchInput, recipes) {
+    searchInput.addEventListener("input", e => {
+        const value = e.target.value;
+        recipes.forEach(recipe => {
+            const isVisible = recipe.name.toLowerCase().includes(value.toLowerCase());
+            recipe.element.classList.toggle("hide", !isVisible)
+        })
+    });
+}
+
+const recipeHTMLList = document.getElementById('recipe-list');
+const recipeListChildren = recipeHTMLList.children;
+var recipes = []; 
+var recipeList = createRecipeList(recipeListChildren, recipes);
+const searchInput = document.querySelector('#search');
+
+recipeSearch(searchInput, recipes);
