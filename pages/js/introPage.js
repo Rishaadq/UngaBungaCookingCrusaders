5c65f1cec3540b0509717eea7a36b77881e7b582
Rishaadq <rishaadq@gmail.com>
2022-07-27 21:33:53 +0300

    Added transitions for daal page

--- pages/js/introPage.js
+++ pages/js/introPage.js
@@ -1,10 +1,25 @@
 //const swup = new Swup();
 window.onload = () => {
     const transition_el = document.querySelector('.transition');
+    const anchors = document.querySelectorAll('a');
     setTimeout(() => {
         transition_el.classList.remove('is-active');
-    }, 500);
+    }, 125);
+
+    for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            let target = e.target.href;
+            
+            transition_el.classList.add('is-active');
+            setTimeout(() => {
+                window.location.href = target;
+            }, 125);
+        })
+    }
 }
+
 
 
 function createRecipeList(recipeListCollection, recipes) {
