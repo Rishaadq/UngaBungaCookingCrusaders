900db07056a606b6489181d3f967da875b5f2ed9
Justin Chun <chunny0501@gmail.com>
2022-07-29 10:47:13 -0400

    Modified transition.

--- pages/js/introPage.js
+++ pages/js/introPage.js
@@ -1,4 +1,23 @@
-const swup = new Swup();
+window.onload = () => {
+    const transition_el = document.querySelector('.transition');
+    const anchors = document.querySelectorAll('a');
+    setTimeout(() => {
+        transition_el.classList.remove('is-active');
+    }, 125);
+
+    for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            let target = e.target.href;
+            
+            transition_el.classList.add('is-active');
+            setTimeout(() => {
+                window.location.href = target;
+            }, 125);
+        })
+    }
+}
 
 function createRecipeList(recipeListCollection, recipes) {
     for(i= 0; i < recipeListCollection.length; i++) {
